dd23bca0f62c55f14623be0bdc6026256d2c3498
Shubham Awasthi <aw.shubh@gmail.com>
2021-03-07 16:26:42 +0530

    Undirect graph builder and parser

---
+++ src/graph-builder/undirectedGraph.ts
@@ -0,0 +1,36 @@
+class Graph{
+    nodes:{id:number, label: string}[]=[];
+    edges:{from:number, to: number}[]=[];
+    N;
+    // network;
+    constructor(n:number,e:number[][]){
+        for(let i=0; i<n; i++){
+            this.nodes.push({ id: i+1, label: (i+1).toString() })
+        }
+        if(e){
+            e.forEach(edge => {
+                this.edges.push({ from: edge[0], to: edge[1] });
+            });
+        }
+        this.N=n;
+    }
+    addEdge(a:number,  b:number){
+        if(a>0 && a<=this.N && b>0 && b<=this.N){
+            this.edges.push({ from: a, to: b });
+        }
+    }
+    getEdges(){ return this.edges; }
+    getNodes(){ return this.nodes; }
+    create(){
+        // create a netyarnwork
+        // var container = document.getElementById("mynetwork");
+        // var data = {
+        //     nodes: new vis.DataSet(this.nodes),
+        //     edges: new vis.DataSet(this.edges),
+        // };
+        // var options = {};
+        // this.network = new vis.Network(container, data, options);
+    }
+}
+
+export default Graph;
---
+++ src/parser/adjacencyList.ts
@@ -0,0 +1,17 @@
+class AdjacencyList{
+    edges:number[][];
+    N:number;
+    constructor(S: string){
+        let l:number[][]=JSON.parse(S);
+        this.edges=[]
+        for(let i=0; i<l.length; i++){
+            for(let j=0; j<l[i].length; j++){
+                this.edges.push([i+1, l[i][j]]);
+            }
+        }
+        this.N=l.length;
+    }
+    getEdges():number[][]{return this.edges;}
+    getNumberOfNodes():number{return this.N;}
+}
+export default AdjacencyList;
