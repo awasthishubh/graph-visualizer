daef9476486724180f27a83dc5663c1b27f8f4c8
Shubham Awasthi <aw.shubh@gmail.com>
2021-03-21 15:55:56 +0530

    Fix reload button

--- src/component/graph.tsx
+++ src/component/graph.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Graph from 'vis-react';
 
-function GraphElement({ inputString, graph, inputFormat, isDirected, setShowGraph,isWeighted,is0 }: any) {
+function GraphElement({ inputString, graph, inputFormat, isDirected, setShowGraph, isWeighted, is0 }: any) {
   return (
     <div className="App" style={{ height: "100%" }}>
-      <button className="center-button btn btn-primary" onClick={() => setShowGraph(false)}><span className="reload">&#x21bb;</span></button>
-      <div style={{textAlign: "center"}}>
-      <b>Input Type:</b> {inputFormat==="edg"?"Edges List":"Adjacency List"}&emsp;&emsp;
-      <b>Graph Type:</b> {isDirected?"Directed":"Undirected"}&emsp;&emsp;
-      <b>isWeighted:</b>{isWeighted?"True":"False"}&emsp;&emsp;
-      <b>Start Index:</b>{is0?"0":"1"} <br/>
-      <b>Input</b> {inputString}
+      <div style={{ textAlign: "center", height: 55 }}>
+        <b>Input Type:</b> {inputFormat === "edg" ? "Edges List" : "Adjacency List"}&emsp;&emsp;
+        <b>Graph Type:</b> {isDirected ? "Directed" : "Undirected"}&emsp;&emsp;
+        <b>isWeighted:</b>{isWeighted ? "True" : "False"}&emsp;&emsp;
+        <b>Start Index:</b>{is0 ? "0" : "1"} <br />
+        <b>Input</b> {inputString}
       </div>
+      {/* <div> */}
+      <div style={{height: "calc(100% - 105px)"}}>
       {/* @ts-ignore */}
       <Graph
         options={{
@@ -25,6 +26,10 @@ function GraphElement({ inputString, graph, inputFormat, isDirected, setShowGrap
         }}
         graph={graph}
       />
+      </div>
+      <div style={{ textAlign: "center", height: 20 }}>
+        <button className="center-button btn btn-primary" onClick={() => setShowGraph(false)}><span className="reload">&#x21bb;</span></button>
+      </div>
     </div>
   );
 }
